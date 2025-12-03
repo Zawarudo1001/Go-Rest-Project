c4a77df475e24538b9c59e20dda9b66c61bba536
Zawarudo1001 <timohinvaceslav4@gmail.com>
2025-12-03 12:01:04 +0300

    added some changes

--- cmd/main.go
+++ cmd/main.go
@@ -4,6 +4,23 @@ import (
 	"fmt"
 )
 
+type standart struct {
+	Name string
+	Age  int
+}
+
+func initStandart(name string, age int) standart {
+	return standart{
+		Name: name,
+		Age:  age,
+	}
+}
+
+func (s standart) Speak() {
+	fmt.Println("My name is", s.Name, "and I am", s.Age, "years old.")
+}
+
 func main() {
-	fmt.Println("Hello world")
+	a := initStandart("Alice", 30)
+	a.Speak()
 }
